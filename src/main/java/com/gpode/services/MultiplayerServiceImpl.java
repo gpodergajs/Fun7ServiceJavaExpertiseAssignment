155b81caeaddba8f366d4d9057f572427eb221c7
Gašper Podergajs <gpodergajs@gmail.com>
2020-02-16 18:21:07 +0100

    Update MultiplayerServiceImpl.java
    Refactor name

--- src/main/java/com/gpode/services/MultiplayerServiceImpl.java
+++ src/main/java/com/gpode/services/MultiplayerServiceImpl.java
@@ -3,7 +3,7 @@ package com.gpode.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MutliplayerServiceImpl implements MultiplayerService {
+public class MultiplayerServiceImpl implements MultiplayerService {
 
     public boolean getMultiplayerServiceStatus(int apiCalls) {
         return apiCalls >= 5 ? true : false;
